d779eb31539dce043c1160ed4b63476af8f7b9a1
student814 <gjtjdo2000@gmail.com>
2023-06-24 14:28:56 +0900

    20230624 정리

--- app/home/index.js
+++ app/home/index.js
@@ -1,35 +1,31 @@
 import { StyleSheet, Text, View } from "react-native";
-import { Link, withLayoutContext } from 'expo-router'
+import { Link } from 'expo-router'
 
 export default function Page() {
     return (
         <View style={home.container}>
             <Text style={home.map}>지도 api</Text>
             <Link href="/filter">
-                <View style={{
-                    flex: 1,
-                    flexDirection: "row-reverse"
-                }}>
-                    <Text style={home.filter}>
-                        필터
-                    </Text>
-                </View>
+                <Text style={home.filter}>
+                    필터
+                </Text>
             </Link>
             <View>
-                <Text style={underbar.bar}>
-                    <Link href="price">
-                        <Text style={underbar.round}>시세</Text>
-                    </Link>
-                    <Link href="/map">
-                        <Text style={underbar.round}>지도</Text>
-                    </Link>
-                    <Text style={underbar.main}>홈</Text>
-                    <Link href="/search">
-                        <Text style={underbar.round}>검색</Text>
-                    </Link>
-                    <Link href="/option">
-                        <Text style={underbar.round}>설정</Text>
-                    </Link>
+                <Text style={home.infocase}>정보</Text>
+            </View>
+            <View style={underbar.bar}>
+                <Text style={underbar.round}>
+                    <Link href="price">시세</Link>
+                </Text>
+                <Text style={underbar.round}>
+                    <Link href="/map">지도</Link>
+                </Text>
+                <Text style={[underbar.round, underbar.main]}>홈</Text>
+                <Text style={underbar.round}>
+                    <Link href="/search">검색</Link>
+                </Text>
+                <Text style={underbar.round}>
+                    <Link href="/option">설정</Link>
                 </Text>
             </View>
         </View>
@@ -39,44 +35,54 @@ export default function Page() {
 const home = StyleSheet.create({
     container: {
         flex: 1,
-        textAlign: "center",
     },
     map: {
         width: "100vw",
         height: "60vh",
-        backgroundColor: "lightblue"
+        backgroundColor: "lightblue",
+
     },
     filter: {
         backgroundColor: "gray",
-        width: 40,
+        width: 70,
         height: 40,
-        margin: 20,
-        padding: 10,
-        fontSize: 10,
-        left: "70vw",
+        padding: 5,
+        fontSize: 20,
+        top: "80vh",
+        left: "75vw",
         position: "fixed",
+        textAlign: "center",
     },
+    infocase: {
+        width: "100vw",
+        height: "30vh",
+    },
+    info: {
+        color: "gray",
+        width: "100vw",
+        height: "20vh",
+    }
 })
 const underbar = StyleSheet.create({
     bar: {
-        backgroundColor: "rgba(50, 50, 50, 0.1)",
+        flex: 1,
         height: "10vh",
-        bottom: 0,
-        position: "fixed",
-        width: "100vw"
+        width: "100vw",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        backgroundColor: "rgba(30, 30, 30, 0.1)"
     },
     round: {
         backgroundColor: "lightblue",
-        padding: 20,
-        borderRadius: 100,
-        margin: "1%",
+        borderRadius: "50%",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center", 
+        alignItems: "center", 
     },
     main: {
         backgroundColor: "blue",
         color: "white",
-        fontSize: 20,
-        padding: 30,
-        borderRadius: 100,
-        margin: "2%",
     }
 })
--- app/map/index.js
+++ app/map/index.js
@@ -1,9 +1,90 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Link } from 'expo-router'
 
 export default function Page() {
     return (
-        <View>
-            
+        <View style={map.container}>
+            <View style={map.header}>
+                <Text style={map.filter}>
+                    <Link href={"/filter"}>
+                        필터
+                    </Link>
+                </Text>
+                <Text style={map.search}>
+                    검색
+                </Text>
+            </View>
+            <Text style={map.map}>
+                지도 api
+            </Text>
+            <View style={underbar.bar}>
+                <Text style={underbar.round}>
+                    <Link href="price">시세</Link>
+                </Text>
+                <Text style={underbar.round}>
+                    <Link href="/map">지도</Link>
+                </Text>
+                <Text style={[underbar.round, underbar.main]}>홈</Text>
+                <Text style={underbar.round}>
+                    <Link href="/search">검색</Link>
+                </Text>
+                <Text style={underbar.round}>
+                    <Link href="/option">설정</Link>
+                </Text>
+            </View>
+
         </View>
     );
 }
+
+const map = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    map: {
+        width: "100vw",
+        height: "85vh",
+        backgroundColor: "lightblue"
+    },
+    header: {
+        display: "flex",
+        flexDirection: "row"
+    },
+    filter: {
+        backgroundColor: "rgba(30, 30, 30, 0.3)",
+        width: "20vw",
+        height: "5vh",
+        textAlign: "center",
+        display:"flex",
+        alignItems:"center",
+    },
+    search: {
+        height: "5vh",
+        width: "80vw",
+        display:"flex",
+        alignItems:"center",
+    }
+})
+const underbar = StyleSheet.create({
+    bar: {
+        flex: 1,
+        height: "10vh",
+        width: "100vw",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        backgroundColor: "rgba(30, 30, 30, 0.1)"
+    },
+    round: {
+        backgroundColor: "lightblue",
+        borderRadius: "50%",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    main: {
+        backgroundColor: "blue",
+        color: "white",
+    }
+})
